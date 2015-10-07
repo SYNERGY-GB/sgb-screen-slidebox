ac9955b568402abe31625d2015ac598e1b7db111
Saul Hernandez <sauldhernandez@gmail.com>
2015-10-06 21:55:15 -0430

    Position setting test

--- dist/sgb-screen-slidebox.js
+++ dist/sgb-screen-slidebox.js
@@ -5,11 +5,9 @@ angular.module('sgb-screen-slidebox', ['megazord'])
     .controller('sgb-screen-slidebox-controller', ['$scope', '_router', '_screen', '_screenParams', 'lodash', '$ionicSlideBoxDelegate', function ($scope, _router, _screen, _screenParams, _, $ionicSlideBoxDelegate) {
         _screen.initialize($scope, _screenParams);
 
-        $scope.currentSlide = _screenParams.defaultSlide === undefined ? 0 : _screenParams.defaultSlide;
-
-        $scope.update = function (index) {
-            $scope.currentSlide = index;
-        };
+        if(!$scope.currentSlide) {
+            $scope.currentSlide = _screenParams.defaultSlide === undefined ? 0 : _screenParams.defaultSlide;
+        }
     }]);
 
 },{}]},{},[1]);
--- src/sgb-screen-slidebox.js
+++ src/sgb-screen-slidebox.js
@@ -4,9 +4,7 @@ angular.module('sgb-screen-slidebox', ['megazord'])
     .controller('sgb-screen-slidebox-controller', ['$scope', '_router', '_screen', '_screenParams', 'lodash', '$ionicSlideBoxDelegate', function ($scope, _router, _screen, _screenParams, _, $ionicSlideBoxDelegate) {
         _screen.initialize($scope, _screenParams);
 
-        $scope.currentSlide = _screenParams.defaultSlide === undefined ? 0 : _screenParams.defaultSlide;
-
-        $scope.update = function (index) {
-            $scope.currentSlide = index;
-        };
+        if(!$scope.currentSlide) {
+            $scope.currentSlide = _screenParams.defaultSlide === undefined ? 0 : _screenParams.defaultSlide;
+        }
     }]);
