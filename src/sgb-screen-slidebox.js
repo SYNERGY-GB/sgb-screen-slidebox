263b13330e3490cdf1771e52fbab2654b9dcc1c1
Saul Hernandez <sauldhernandez@gmail.com>
2015-10-06 21:42:00 -0430

    Changed the way the default slide is set

--- src/sgb-screen-slidebox.js
+++ src/sgb-screen-slidebox.js
@@ -4,9 +4,5 @@ angular.module('sgb-screen-slidebox', ['megazord'])
     .controller('sgb-screen-slidebox-controller', ['$scope', '_router', '_screen', '_screenParams', 'lodash', '$ionicSlideBoxDelegate', function ($scope, _router, _screen, _screenParams, _, $ionicSlideBoxDelegate) {
         _screen.initialize($scope, _screenParams);
 
-        var slidebox = $ionicSlideBoxDelegate.$getByHandle('mainSlidebox');
-
-        if(_screenParams.defaultSlide !== undefined) {
-            slidebox.slide(_screenParams.defaultSlide, 0);
-        }
+        $scope.currentSlide = _screenParams.defaultSlide === undefined ? 0 : _screenParams.defaultSlide;
     }]);
