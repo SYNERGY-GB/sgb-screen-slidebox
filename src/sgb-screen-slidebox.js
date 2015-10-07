07b675286a8609c705036b25dd66d2160a934eab
Saul Hernandez <sauldhernandez@gmail.com>
2015-10-06 21:56:41 -0430

    Position setting test

--- src/sgb-screen-slidebox.js
+++ src/sgb-screen-slidebox.js
@@ -4,6 +4,7 @@ angular.module('sgb-screen-slidebox', ['megazord'])
     .controller('sgb-screen-slidebox-controller', ['$scope', '_router', '_screen', '_screenParams', 'lodash', '$ionicSlideBoxDelegate', function ($scope, _router, _screen, _screenParams, _, $ionicSlideBoxDelegate) {
         _screen.initialize($scope, _screenParams);
 
+        console.log('Loading slider...');
         if(!$scope.currentSlide) {
             $scope.currentSlide = _screenParams.defaultSlide === undefined ? 0 : _screenParams.defaultSlide;
         }
